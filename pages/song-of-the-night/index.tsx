9ce84f49e1939fb993eec481e53cef01d7644078
Jason <jsnphil@gmail.com>
2023-03-12 17:35:33 -0500

    Completed SOTN winners page

--- pages/song-of-the-night/index.tsx
+++ pages/song-of-the-night/index.tsx
@@ -1,7 +1,8 @@
 import { NextPage } from 'next';
 import { useState } from 'react';
-import { Accordion, Form } from 'react-bootstrap';
+import { Accordion, Col, Container, Form, Row } from 'react-bootstrap';
 import styles from './sotn.module.css';
+import sotnData from '../../data/sotn-response.json';
 
 type SotnProps = {
   data: SotnWinner[];
@@ -15,7 +16,7 @@ type SotnWinner = {
   readonly year: string;
   readonly featuredArtist: string;
   readonly sotsWinner: boolean;
-  readonly streamDate: string;
+  readonly playDate: string;
   readonly title?: string;
 };
 
@@ -33,6 +34,7 @@ export async function getServerSideProps() {
     `https://${kentobotApiHost}/dev/song-of-the-night/winners`
   );
   const data = await res.json();
+  // const data = JSON.parse(JSON.stringify(sotnData));
 
   let winnersMap = new Map<string, any[]>();
 
@@ -51,8 +53,7 @@ export async function getServerSideProps() {
   winnersMap.forEach((songs, key) => {
     songs.sort(
       (song1: SotnWinner, song2: SotnWinner) =>
-        new Date(song2.streamDate).getTime() -
-        new Date(song1.streamDate).getTime()
+        new Date(song2.playDate).getTime() - new Date(song1.playDate).getTime()
     );
 
     sotnWinners.push({
@@ -87,14 +88,80 @@ const SongOfTheNightStandings: NextPage = ({ winners }) => {
     seasons.push(season);
   }
 
+  function getDate(playDate: string) {
+    const date = new Date(playDate);
+
+    const monthNames = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ];
+
+    return `${
+      monthNames[date.getMonth()]
+    } ${date.getDate()}, ${date.getFullYear()}`;
+  }
+
   return (
     <>
       <main>
         <div className='container d-xl-none d-xl-block mb-5 mt-5  aligns-items-center justify-content-center'>
-          Season DropDown
+          <div className='container mb-5 mt-5 aligns-items-center justify-content-center'>
+            <Form>
+              <Form.Group
+                className='mb5 form-inline'
+                controlId='seasonSelectorControl'
+              >
+                <Form.Label>Season:</Form.Label>
+
+                <Form.Select id='seasonSelector' onChange={handleMenuChange}>
+                  {seasons.map((season) => (
+                    <option key={season} value={season}>
+                      {season}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Form.Group>
+            </Form>
+          </div>
+
+          <Accordion defaultActiveKey='0'>
+            {winners.map((winner: SotnWinnerData, index: string) => (
+              <Accordion.Item eventKey={index} key={index}>
+                <Accordion.Header>
+                  {winner.username} - {winner.songs.length}{' '}
+                  {winner.songs.length == 1 ? 'win' : 'wins'}
+                </Accordion.Header>
+                <Accordion.Body>
+                  {winner.songs!.map((song: SotnWinner, index: number) => (
+                    <Row key={index}>
+                      <Col>
+                        <a
+                          href={`https://youtu.be/${song.youtubeId}`}
+                          target='_blank'
+                        >
+                          {song.title}
+                        </a>
+                      </Col>
+                      <Col>{getDate(song.playDate)}</Col>
+                    </Row>
+                  ))}
+                </Accordion.Body>
+              </Accordion.Item>
+            ))}
+          </Accordion>
         </div>
         <div className='d-none d-xl-block mb-5'>
-          <div className='container d-flex aligns-items-center justify-content-center'>
+          <div className='container d-flex aligns-items-center justify-content-center mb-5'>
             <div id='menuTop' className='innerContainer'>
               <button
                 name='season12'
@@ -229,58 +296,49 @@ const SongOfTheNightStandings: NextPage = ({ winners }) => {
               </button>
             </div>
           </div>
-        </div>
-        {/* <div className='container mb-5 mt-5 aligns-items-center justify-content-center'>
-          <Form>
-            <Form.Group
-              className='mb5 form-inline'
-              controlId='seasonSelectorControl'
-            >
-              <Form.Label>Season:</Form.Label>
 
-              <Form.Select id='seasonSelector' onChange={handleMenuChange}>
-                {seasons.map((season) => (
-                  <option key={season} value={season}>
-                    {season}
-                  </option>
+          <Container>
+            <div className='pb-3'>
+              <Row className={`${styles.winnersheading} ${styles.heading}`}>
+                <Col className='roundTL' xs={4}>
+                  Song Title
+                </Col>
+                <Col xs={4}>Artist</Col>
+                <Col>Year</Col>
+                <Col>Stream Date</Col>
+              </Row>
+            </div>
+            {winners.map((winner: SotnWinnerData, index: string) => (
+              <div className='pb-5 rounded-circle'>
+                <Row>
+                  <Col className={`${styles.userHeading} ${styles.heading}`}>
+                    {winner.username} - {winner.songs.length}{' '}
+                    {winner.songs.length == 1 ? 'win' : 'wins'}
+                  </Col>
+                </Row>
+                {winner.songs!.map((song: SotnWinner, index: number) => (
+                  <div className={`${styles.winnerRow}`}>
+                    <Row>
+                      <Col xs={4}>
+                        <a
+                          href={`https://youtu.be/${song.youtubeId}`}
+                          target='_blank'
+                        >
+                          {song.title}
+                        </a>
+                      </Col>
+                      <Col xs={4}>
+                        {getArtistValue(song.artist, song.featuredArtist)}
+                      </Col>
+                      <Col>{song.year}</Col>
+                      <Col>{getDate(song.playDate)}</Col>
+                    </Row>
+                  </div>
                 ))}
-              </Form.Select>
-            </Form.Group>
-          </Form>
-        </div> */}
-        <p>Standings for Season {seasonState}</p>
-        <p>Number of winners: {winners.length}</p>
-
-        {/* <pre>{JSON.stringify(winners, null, 2)}</pre> */}
-
-        <Accordion defaultActiveKey='0'>
-          {winners.map((winner: SotnWinnerData, index: string) => (
-            <Accordion.Item eventKey={index} key={index}>
-              <Accordion.Header>
-                {winner.username} - {winner.songs.length} wins
-              </Accordion.Header>
-              <Accordion.Body>
-                <table>
-                  <tbody>
-                    <>
-                      {console.log(winner.songs)}
-
-                      {winner.songs!.map((song: SotnWinner, index: number) => (
-                        <tr key={index}>
-                          <td>{song.title}</td>
-                          <td>
-                            {getArtistValue(song.artist, song.featuredArtist)}
-                          </td>
-                          <td>{song.streamDate}</td>
-                        </tr>
-                      ))}
-                    </>
-                  </tbody>
-                </table>
-              </Accordion.Body>
-            </Accordion.Item>
-          ))}
-        </Accordion>
+              </div>
+            ))}
+          </Container>
+        </div>
       </main>
     </>
   );
