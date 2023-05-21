49639f9845504d2c87bf575778fb18883b288fad
jsnphil <jsnphil@users.noreply.github.com>
2023-05-21 18:29:37 -0500

    Making stage path environment dynamic (#52)

--- utils/kentobotApi.ts
+++ utils/kentobotApi.ts
@@ -2,11 +2,13 @@ import axios from 'axios';
 import useSWR from 'swr';
 
 const baseUrl = process.env.NEXT_PUBLIC_KENTOBOT_API_HOST;
+const env = process.env.NEXT_PUBLIC_ENVIRONMENT;
+
 const fetcher = async (url: string) =>
   await axios.get(url).then((res) => res.data);
 
 export const useKentobot = (path: string) => {
-  const url = `https://${baseUrl}/dev/${path}`;
+  const url = `https://${baseUrl}/${env}/${path}`;
 
   const { data, error, isLoading } = useSWR(url, fetcher);
 
