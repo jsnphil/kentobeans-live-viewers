31725ad83375d728f1522afed351cd648f37e123
jsnphil <jsnphil@users.noreply.github.com>
2023-05-21 21:33:58 -0500

    Escaping quotes

--- pages/about.tsx
+++ pages/about.tsx
@@ -21,34 +21,37 @@ const About: NextPage = () => {
                 <Col>Streaming</Col>
               </Row>
               <Row>
-                <Col>Gretsch Renown Maple drum set (10", 12", 14", 22")</Col>
+                <Col>
+                  Gretsch Renown Maple drum set (10&quot;, 12&quot;, 14&quot;,
+                  22&quot;)
+                </Col>
                 <Col>Focusrite Clarett 8Pre</Col>
               </Row>
               <Row>
                 <Col>
-                  DW Collector's Series Black Nickel over Brass 6.5"x14" snare
-                  drum
+                  DW Collector's Series Black Nickel over Brass 6.5&quot; x
+                  14&quot; snare drum
                 </Col>
                 <Col>Sony a6000</Col>
               </Row>
               <Row>
-                <Col>Zildjian K Custom Hybrid 14.25" hi-hats</Col>
+                <Col>Zildjian K Custom Hybrid 14.25&quot; hi-hats</Col>
                 <Col>Logitech C920</Col>
               </Row>
               <Row>
-                <Col>Zildjian K Custom Hybrid 17" crash cymbal</Col>
-                <Col>GVM 48" camera slider</Col>
+                <Col>Zildjian K Custom Hybrid 17&quot; crash cymbal</Col>
+                <Col>GVM 48&quot; camera slider</Col>
               </Row>
               <Row>
-                <Col>Zildjian K Custom Hybrid 21" ride cymbal</Col>
+                <Col>Zildjian K Custom Hybrid 21&quot; ride cymbal</Col>
                 <Col />
               </Row>
               <Row>
-                <Col>Zildjian K Custom Hybrid 21" ride cymbal</Col>
+                <Col>Zildjian K Custom Hybrid 21&quot; ride cymbal</Col>
                 <Col />
               </Row>
               <Row>
-                <Col>Sabian AA 18" medium crash cymbal</Col>
+                <Col>Sabian AA 18&quot; medium crash cymbal</Col>
                 <Col />
               </Row>
               <hr />
