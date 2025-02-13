fb64409bc9c51483218621ca5d2592e876616ab3
jsnphil <jsnphil@kentobot.io>
2025-02-12 19:34:22 -0600

    Roughed in layout

--- components/SongPlayer.tsx
+++ components/SongPlayer.tsx
@@ -3,7 +3,6 @@ import SongPlayerQueue from './SongPlayerQueue';
 export default function SongPlayer() {
   return (
     <div>
-      <h1>SongPlayer</h1>
       <SongPlayerQueue />
     </div>
   );
--- components/SongPlayerQueue.tsx
+++ components/SongPlayerQueue.tsx
@@ -8,6 +8,30 @@ import YouTube, {
   YouTubeProps
 } from 'react-youtube';
 import { set } from 'lodash';
+import {
+  Button,
+  ButtonGroup,
+  ButtonToolbar,
+  Col,
+  Container,
+  Form,
+  Row
+} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faStar,
+  faTicket,
+  faDice,
+  faPlay,
+  faPause,
+  faStop,
+  faVolumeMute,
+  faVolumeHigh,
+  faVolumeDown,
+  faVolumeUp,
+  faForward,
+  faForwardFast
+} from '@fortawesome/free-solid-svg-icons';
 
 interface QueueInfo {
   status: 'Closed|Open';
@@ -22,11 +46,11 @@ interface QueueInfo {
 interface SongRequest {
   youtubeId: string;
   title: string;
-  length: number;
+  length?: number;
   requestedBy: string;
-  isBumped: boolean;
-  isShuffled: boolean;
-  isShuffleEntered: boolean;
+  isBumped?: boolean;
+  isShuffled?: boolean;
+  isShuffleEntered?: boolean;
 }
 
 /*
@@ -54,9 +78,9 @@ export default function SongPlayerQueue() {
   const [remainingBeanBumps, setRemainingBeanBumps] = useState(0);
 
   const [songList, setSongList] = useState<SongRequest[]>([]);
-  const [songHistory, setSongHistory] = useState<SongListItem[]>([]);
-  const [currentSong, setCurrentSong] = useState<SongListItem>();
-  const [contenders, setContenders] = useState<SongListItem[]>([]);
+  const [songHistory, setSongHistory] = useState<SongRequest[]>([]);
+  const [currentSong, setCurrentSong] = useState<SongRequest>();
+  const [contenders, setContenders] = useState<SongRequest[]>([]);
 
   //Public API that will echo messages sent to it back to the client
   const [socketUrl, setSocketUrl] = useState(
@@ -81,6 +105,9 @@ export default function SongPlayerQueue() {
         if (message.songQueue) {
           setSongList(message.songQueue);
         }
+        if (message.currentSong) {
+          setCurrentSong(message.currentSong);
+        }
       },
       shouldReconnect: (closeEvent) => true,
       heartbeat: {
@@ -102,6 +129,7 @@ export default function SongPlayerQueue() {
   const [player, setPlayer] = useState<YouTubePlayer>(null);
   const [playerVolume, setPlayerVolume] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [queueOpen, setQueueOpen] = useState(false);
 
   const onReady = (event: YouTubeEvent) => {
     setPlayer(event.target);
@@ -143,39 +171,143 @@ export default function SongPlayerQueue() {
     setIsPlaying(false);
   };
 
+  const nextSongHandler = () => {
+    console.log('Next Song');
+    sendJsonMessage({
+      action: 'sendmessage',
+      message: 'songqueue:next'
+    });
+  };
+
   return (
-    <div>
-      <YouTube
-        videoId={songList[0]?.youtubeId}
-        onReady={onReady}
-        title='Kentobot Player'
-        opts={{
-          playerVars: {
-            controls: 0
-          }
-        }}
-      />
-      <h1>Queue</h1>
+    <Container>
+      <div>
+        <Row>
+          <Col md={6}>
+            <YouTube
+              videoId={currentSong?.youtubeId}
+              onReady={onReady}
+              title='Kentobot Player'
+              opts={{
+                playerVars: {
+                  controls: 0
+                }
+              }}
+            />
+          </Col>
+          <Col md={6}>
+            <Row>
+              <Col>Requests Enabled</Col>
+              <Col>
+                <Form.Check // prettier-ignore
+                  type='switch'
+                  id='custom-switch'
+                  onChange={(e) => {
+                    // TODO Send message to WSS to toggle queue
+                    if (queueOpen) {
+                      console.log('Closing queue');
+                      setQueueOpen(false);
+                    } else {
+                      console.log('Opening queue');
+                      setQueueOpen(true);
+                    }
+                  }}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>Songs requested</Col>
+              <Col>{songList.length}</Col>
+            </Row>
+            <Row>
+              <Col>Bumped songs</Col>
+              <Col>5</Col>
+            </Row>
+            <Row>
+              <Col>Played Songs</Col>
+              <Col>{playedSongs}</Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button variant='primary'>Start Stream</Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button variant='primary'>Start Shaffle</Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
 
-      {isPlaying ? (
-        <button onClick={onPauseHandler}>Pause</button>
-      ) : (
-        <button onClick={onPlayHandler}>Play</button>
-      )}
+        <ButtonToolbar aria-label='Toolbar with button groups'>
+          <ButtonGroup size='lg' aria-label='Basic example'>
+            {isPlaying ? (
+              <Button variant='primary' title='Pause' onClick={onPauseHandler}>
+                <FontAwesomeIcon icon={faPause} />
+              </Button>
+            ) : (
+              <Button title='Play' onClick={onPlayHandler}>
+                <FontAwesomeIcon icon={faPlay} />
+              </Button>
+            )}
+            <Button title='Stop' onClick={stopHandler}>
+              <FontAwesomeIcon icon={faStop} />
+            </Button>
+            <Button title='Next Song' onClick={nextSongHandler}>
+              <FontAwesomeIcon icon={faForwardFast} />
+            </Button>
+          </ButtonGroup>
+        </ButtonToolbar>
 
-      <button onClick={volumnUpHandler}>Volume Up</button>
-      <button onClick={volumnDownHandler}>Volume Down</button>
-      <button onClick={muteHandler}>Mute</button>
-      <button onClick={stopHandler}>Stop</button>
-      <button>Open Shuffle</button>
-      <button>Next Song</button>
-      <p>Volume: {playerVolume}</p>
-      {songList.map((song) => (
-        <div key={song.youtubeId}>
-          <h2>{song.title}</h2>
-          <p>Requested by: {song.requestedBy}</p>
+        <ButtonToolbar aria-label='Toolbar with button groups'>
+          <ButtonGroup size='lg' aria-label='Basic example'>
+            <Button title='Volume Up' onClick={volumnUpHandler}>
+              <FontAwesomeIcon icon={faVolumeUp} />
+            </Button>
+            <Button title='Volume Down' onClick={volumnDownHandler}>
+              <FontAwesomeIcon icon={faVolumeDown} />
+            </Button>
+            <Button title='Mute' onClick={muteHandler}>
+              <FontAwesomeIcon icon={faVolumeMute} />
+            </Button>
+          </ButtonGroup>
+        </ButtonToolbar>
+
+        <p>Volume: {playerVolume}%</p>
+
+        <div id='nowPlaying' className='mb-3'>
+          <Row>
+            <Col className='subheading roundedTopLeft roundedBottomLeft roundedTopRight roundedBottomRight text-center'>
+              Now Playing
+            </Col>
+          </Row>
+          <Row>
+            {currentSong ? (
+              <>
+                <Col md={9}>{currentSong.title}</Col>
+                <Col md={2}>{currentSong.requestedBy}</Col>
+                <Col md={1}>{currentSong.length}</Col>
+              </>
+            ) : (
+              <Col md={12}>Nothing playing</Col>
+            )}
+          </Row>
         </div>
-      ))}
-    </div>
+        <Row>
+          <Col className='subheading roundedTopLeft roundedBottomLeft roundedTopRight roundedBottomRight text-center'>
+            Request Queue
+          </Col>
+        </Row>
+
+        {songList.map((song) => (
+          <Row key={song.youtubeId} className='songTableRow'>
+            <Col md={9}>{song.title}</Col>
+            <Col md={2}>{song.requestedBy}</Col>
+            <Col md={1}>{song.length}</Col>
+          </Row>
+        ))}
+      </div>
+    </Container>
   );
 }
